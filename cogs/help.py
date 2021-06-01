343062004d714950a887492b07f0fabd909a8f38
Guard-SK <81436587+Guard-SK@users.noreply.github.com>
2021-06-01 09:47:32 +0200

    help dialog fix

--- cogs/help.py
+++ cogs/help.py
@@ -21,6 +21,7 @@ class Help(Cog):
         embed=Embed(title="HELP", description="Welcome to the Subdroid help dialog! \n Prefix = sub", color=0x15cb55, timestamp=datetime.utcnow())
         embed.add_field(name="----Info----", value="Subdroid is a chatting bot. He has bunch of custom and automacitated messages that you can play around with. He also can find something on wikipeida for you! Just try him out! Commands are below.")
         embed.add_field(name="-----Commands-----", value="```droid``` - when you type subdroid [content] to chat with subdroid. When you type ```subdroid Do you know about|what is|who is|tell me about [content]``` he will find [content] on wikipedia for you. \n ```ping``` - ping command", inline=False)
+        await ctx.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_ready(self):
